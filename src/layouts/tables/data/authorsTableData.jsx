0f1870ddc14509c5e80a7ddf5c92c56b65614a02
andis <andisabdullah2@gmail.com>
2023-06-14 23:26:31 +0800

    ini dari andis

---
+++ src/layouts/tables/data/authorsTableData.jsx
@@ -0,0 +1,199 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable react/function-component-definition */
+/**
+=========================================================
+* Material Dashboard 2 React - v2.2.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/material-dashboard-react
+* Copyright 2023 Creative Tim (https://www.creative-tim.com)
+
+Coded by www.creative-tim.com
+
+ =========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+*/
+
+// Material Dashboard 2 React components
+import MDBox from "components/MDBox";
+import MDTypography from "components/MDTypography";
+import MDAvatar from "components/MDAvatar";
+import MDBadge from "components/MDBadge";
+
+// Images
+import team2 from "assets/images/team-2.jpg";
+
+export default function data() {
+  const Kategori = ({ name }) => (
+    <MDBox display="flex" alignItems="center" lineHeight={1}>
+      <MDAvatar name={name} size="sm" />
+      <MDBox ml={2} lineHeight={1}>
+        <MDTypography display="block" variant="button" fontWeight="medium">
+          {name}
+        </MDTypography>
+      </MDBox>
+    </MDBox>
+  );
+
+  const SubKategori = ({ title, description }) => (
+    <MDBox lineHeight={1} textAlign="left">
+      <MDTypography display="block" variant="caption" color="text" fontWeight="medium">
+        {title}
+      </MDTypography>
+      <MDTypography variant="caption">{description}</MDTypography>
+    </MDBox>
+  );
+
+  return {
+    columns: [
+      { Header: "kategori", accessor: "kategori", align: "left" },
+      { Header: "subkategori", accessor: "subkategori", width: "45%", align: "left" },
+      { Header: "status", accessor: "status", align: "center" },
+      { Header: "action", accessor: "action", align: "center" },
+    ],
+
+    rows: [
+      {
+        kategori: <Kategori name="Makanan" deskripsi="john@creative-tim.com" />,
+        subkategori: <SubKategori title="Makanan Pembuka" />,
+        status: (
+          <MDBox ml={-1}>
+            <MDBadge badgeContent="online" color="success" variant="gradient" size="sm" />
+          </MDBox>
+        ),
+        action: (
+          <MDTypography component="a" href="#" variant="caption" color="text" fontWeight="medium">
+            Edit
+          </MDTypography>
+        ),
+      },
+      {
+        kategori: <Kategori name="Makanan" deskripsi="john@creative-tim.com" />,
+        subkategori: <SubKategori title="Makanan Pembuka" />,
+        status: (
+          <MDBox ml={-1}>
+            <MDBadge badgeContent="online" color="success" variant="gradient" size="sm" />
+          </MDBox>
+        ),
+        action: (
+          <MDTypography component="a" href="#" variant="caption" color="text" fontWeight="medium">
+            Edit
+          </MDTypography>
+        ),
+      },
+      {
+        kategori: <Kategori name="Makanan" deskripsi="john@creative-tim.com" />,
+        subkategori: <SubKategori title="Makanan Pembuka" />,
+        status: (
+          <MDBox ml={-1}>
+            <MDBadge badgeContent="online" color="success" variant="gradient" size="sm" />
+          </MDBox>
+        ),
+        action: (
+          <MDTypography component="a" href="#" variant="caption" color="text" fontWeight="medium">
+            Edit
+          </MDTypography>
+        ),
+      },
+      {
+        kategori: <Kategori name="Makanan" deskripsi="john@creative-tim.com" />,
+        subkategori: <SubKategori title="Makanan Pembuka" />,
+        status: (
+          <MDBox ml={-1}>
+            <MDBadge badgeContent="online" color="success" variant="gradient" size="sm" />
+          </MDBox>
+        ),
+        action: (
+          <MDTypography component="a" href="#" variant="caption" color="text" fontWeight="medium">
+            Edit
+          </MDTypography>
+        ),
+      },
+      {
+        kategori: <Kategori name="Makanan" deskripsi="john@creative-tim.com" />,
+        subkategori: <SubKategori title="Makanan Pembuka" />,
+        status: (
+          <MDBox ml={-1}>
+            <MDBadge badgeContent="online" color="success" variant="gradient" size="sm" />
+          </MDBox>
+        ),
+        action: (
+          <MDTypography component="a" href="#" variant="caption" color="text" fontWeight="medium">
+            Edit
+          </MDTypography>
+        ),
+      },
+      {
+        kategori: <Kategori name="Makanan" deskripsi="john@creative-tim.com" />,
+        subkategori: <SubKategori title="Makanan Pembuka" />,
+        status: (
+          <MDBox ml={-1}>
+            <MDBadge badgeContent="online" color="success" variant="gradient" size="sm" />
+          </MDBox>
+        ),
+        action: (
+          <MDTypography component="a" href="#" variant="caption" color="text" fontWeight="medium">
+            Edit
+          </MDTypography>
+        ),
+      },
+      {
+        kategori: <Kategori name="Makanan" deskripsi="john@creative-tim.com" />,
+        subkategori: <SubKategori title="Makanan Pembuka" />,
+        status: (
+          <MDBox ml={-1}>
+            <MDBadge badgeContent="online" color="success" variant="gradient" size="sm" />
+          </MDBox>
+        ),
+        action: (
+          <MDTypography component="a" href="#" variant="caption" color="text" fontWeight="medium">
+            Edit
+          </MDTypography>
+        ),
+      },
+      {
+        kategori: <Kategori name="Makanan" deskripsi="john@creative-tim.com" />,
+        subkategori: <SubKategori title="Makanan Pembuka" />,
+        status: (
+          <MDBox ml={-1}>
+            <MDBadge badgeContent="online" color="success" variant="gradient" size="sm" />
+          </MDBox>
+        ),
+        action: (
+          <MDTypography component="a" href="#" variant="caption" color="text" fontWeight="medium">
+            Edit
+          </MDTypography>
+        ),
+      },
+      {
+        kategori: <Kategori name="Makanan" deskripsi="john@creative-tim.com" />,
+        subkategori: <SubKategori title="Makanan Pembuka" />,
+        status: (
+          <MDBox ml={-1}>
+            <MDBadge badgeContent="online" color="success" variant="gradient" size="sm" />
+          </MDBox>
+        ),
+        action: (
+          <MDTypography component="a" href="#" variant="caption" color="text" fontWeight="medium">
+            Edit
+          </MDTypography>
+        ),
+      },
+      {
+        kategori: <Kategori name="Makanan" deskripsi="john@creative-tim.com" />,
+        subkategori: <SubKategori title="Makanan Pembuka" />,
+        status: (
+          <MDBox ml={-1}>
+            <MDBadge badgeContent="online" color="success" variant="gradient" size="sm" />
+          </MDBox>
+        ),
+        action: (
+          <MDTypography component="a" href="#" variant="caption" color="text" fontWeight="medium">
+            Edit
+          </MDTypography>
+        ),
+      },
+    ],
+  };
+}
